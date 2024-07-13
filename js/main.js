b95d3f4ba633a042e658cff862172f30f738758e
xxxlsyute <ayelensanch@gmail.com>
2024-07-13 13:06:25 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+alert("Importante: Organizacion sin fines de lucro");
+let pais=prompt("Con fines estadisticos nos gustaria que indique su pais de procedencia:");
+console.log("Su pais de procencia es "+pais);
